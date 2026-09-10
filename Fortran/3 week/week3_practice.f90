e3ce9132aec88a3a910e754e84db8ff607ab02dd
Hyuna <olr.or@yonsei.ac.kr>
2026-09-10 18:09:31 +0900

    update
    Refactor week3_project to include functions for data analysis and file I/O. Added subroutines for reading data, calculating mean and standard deviation, and saving results.

--- Fortran/3 week/week3_practice.f90
+++ Fortran/3 week/week3_practice.f90
@@ -2,49 +2,135 @@ program week3_project
     implicit none
 
     integer, parameter :: max_data = 1000
-    integer :: i, n, count_above
-    double precision :: data(max_data)
-    double precision :: sum, mean
-    double precision :: variance, stddev
+    integer :: n
+    double precision :: x(max_data), y(max_data)
+    double precision :: mean, stddev
     double precision :: maximum, minimum
     double precision :: threshold
 
-    print *, 'Number of measurements:'
-    read *, n
+    print *, '========================================'
+    print *, '   SCIENTIFIC DATA ANALYSIS PROGRAM'
+    print *, '========================================'
 
-    if (n <= 0 .or. n > max_data) stop 'Invalid n'
+    ! Part A: Functions and subroutines
+    ! Part B: File input/output
 
-    ! TODO 1: Read n values into data(i).
+    ! TODO 1: Read n and all (x,y) pairs from experiment.dat.
+    
+    call read_data('experiment.dat', x, y, n, max_data)
 
-
-
-    ! TODO 2: Calculate mean using the array.
-
-
-
-    ! TODO 3: Calculate maximum and minimum.
-
-
-
-    ! TODO 4: Calculate variance and stddev.
-
-
+    ! TODO 2: Reuse the analysis procedures from Part A.
+    
+    mean = calc_mean(y, n)
+    stddev = calc_stddev(y, n, mean)
+    
+    call find_max(y, n, maximum)
+    call find_min(y, n, minimum)
 
     print *, 'Enter threshold:'
     read *, threshold
 
-    count_above = 0
-    print *, 'Index       Value'
+    ! TODO 3: Print the threshold-selected data.
+    
+    call print_threshold_data(x, y, n, threshold)
 
-    ! TODO 5: Print index/value for data above threshold
-    ! and count how many values satisfy the condition.
+    print *, 'Number of data     = ', n
+    print *, 'Mean               = ', mean
+    print *, 'Standard deviation = ', stddev
+    print *, 'Maximum            = ', maximum
+    print *, 'Minimum            = ', minimum
 
+    ! TODO 4: Save the same analysis results to result.dat.
+    
+    call save_results('result.dat', n, mean, stddev, maximum, minimum, x, y, threshold)
 
+contains
 
-    print *, 'Mean            = ', mean
-    print *, 'Std. Dev.       = ', stddev
-    print *, 'Maximum         = ', maximum
-    print *, 'Minimum         = ', minimum
-    print *, 'Above threshold = ', count_above
+    subroutine read_data(filename, x, y, n, max_data)
+        implicit none
+        character(len=*), intent(in) :: filename
+        integer, intent(in) :: max_data
+        integer, intent(out) :: n
+        integer :: i
+        double precision, intent(out) :: x(max_data), y(max_data)
+
+        ! TODO 5: Open filename, read n and all (x,y) pairs, then close it.
+        
+        n = 0
+        x = 0.0d0
+        y = 0.0d0
+    end subroutine read_data
+
+    double precision function calc_mean(data, n)
+        implicit none
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: data(n)
+        double precision :: sum
+
+        ! TODO 6: Move the mean calculation into this function.
+        
+        sum = 0.0d0
+        calc_mean = sum
+    end function calc_mean
+
+    double precision function calc_stddev(data, n, mean)
+        implicit none
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: data(n), mean
+        double precision :: variance
+
+        ! TODO 7: Implement the population standard deviation.
+        
+        variance = 0.0d0
+        calc_stddev = 0.0d0
+    end function calc_stddev
+
+    subroutine find_max(data, n, maximum)
+        implicit none
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: data(n)
+        double precision, intent(out) :: maximum
+
+        ! TODO 8: Find the maximum value.
+        
+        maximum = 0.0d0
+    end subroutine find_max
+
+    subroutine find_min(data, n, minimum)
+        implicit none
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: data(n)
+        double precision, intent(out) :: minimum
+
+        ! TODO 9: Find the minimum value.
+        
+        minimum = 0.0d0
+    end subroutine find_min
+
+    subroutine print_threshold_data(x, y, n, threshold)
+        implicit none
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: x(n), y(n), threshold
+
+        ! TODO 10: Print the index, x, and y for every y above threshold.
+        
+    end subroutine print_threshold_data
+
+    subroutine save_results(filename, n, mean, stddev, maximum, minimum, x, y, threshold)
+        implicit none
+        character(len=*), intent(in) :: filename
+        integer, intent(in) :: n
+        integer :: i
+        double precision, intent(in) :: mean, stddev, maximum, minimum
+        double precision, intent(in) :: x(n), y(n), threshold
+
+        ! TODO 11: Write the analysis result and threshold-selected data to result.dat.
+        
+    end subroutine save_results
 
 end program week3_project
